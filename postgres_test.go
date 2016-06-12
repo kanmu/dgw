25d72607802a910a6ff2f34a1990835029c8ea9e
Akira Chiku <akira.chiku@gmail.com>
2016-06-12 12:42:32 +0900

    Add test

--- postgres_test.go
+++ postgres_test.go
@@ -2,14 +2,60 @@ package tdgw
 
 import (
 	"database/sql"
+	"io/ioutil"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
-func testPgSetup(t *testing.T) {
-	conn, err := sql.Open("postgres", "")
+// before running test, create user and database
+// CREATE USER tdgw_test;
+// CREATE DATABASE  tdgw_test OWNER tdgw_test;
+
+func testPgSetup(t *testing.T) (*sql.DB, func()) {
+	conn, err := sql.Open("postgres", "user=tdgw_test dbname=tdgw_test sslmode=disable")
 	if err != nil {
 		t.Fatal(err)
+	}
+	setupSQL, err := ioutil.ReadFile("./sql_test/postgres_setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = conn.Exec(string(setupSQL))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cleanup := func() {
+		conn.Close()
+	}
+	return conn, cleanup
+}
+
+func TestPgLoadColumnDef(t *testing.T) {
+	conn, cleanup := testPgSetup(t)
+	defer cleanup()
+
+	schema := "public"
+	table := "t1"
+	cols, err := PgLoadColumnDef(conn, schema, table)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, c := range cols {
+		t.Logf("%+v", c)
+	}
+}
+
+func TestPgLoadTableDef(t *testing.T) {
+	conn, cleanup := testPgSetup(t)
+	defer cleanup()
+
+	schema := "public"
+	tbls, err := PgLoadTableDef(conn, schema)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, tbl := range tbls {
+		t.Logf("%+v", tbl)
 	}
 }
---
+++ sql_test/postgres_setup.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS t1;
+DROP TABLE IF EXISTS t2;
+
+CREATE TABLE t1 (
+  id bigserial primary key
+  , i integer not null unique
+  , str text not null
+  , t_with_tz timestamp without time zone not null
+  , t_without_tz timestamp with time zone not null
+);
+
+CREATE TABLE t2 (
+  id bigserial primary key
+  , i integer not null unique
+  , str text not null
+  , t_with_tz timestamp without time zone not null
+  , t_without_tz timestamp with time zone not null
+);
