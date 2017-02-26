5821efcd3c67042fbc3b2c8ad8e7f82b0b067c9f
Akira Chiku <akira.chiku@gmail.com>
2017-02-26 16:33:53 +0900

    Add output file option

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
 	connStr         = kingpin.Arg("conn", "PostgreSQL connection string in URL format").Required().String()
-	schema          = kingpin.Flag("schema", "PostgreSQL schema name").Default("public").String()
-	pkgName         = kingpin.Flag("package", "package name").Default("main").String()
-	typeMapFilePath = kingpin.Flag("typemap", "column type and go type map file path").String()
+	schema          = kingpin.Flag("schema", "PostgreSQL schema name").Default("public").Short('s').String()
+	pkgName         = kingpin.Flag("package", "package name").Default("main").Short('p').String()
+	typeMapFilePath = kingpin.Flag("typemap", "column type and go type map file path").Short('t').String()
+	outFile         = kingpin.Flag("output", "output file path").Short('o').String()
 )
 
 func main() {
@@ -26,5 +27,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", src)
+
+	var out os.File
+	if *outFile != "" {
+		out, err := os.Create(*outFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		if _, err := out.Write(src); err != nil {
+			log.Fatal(err)
+		}
+		out.Sync()
+		return
+	}
+	out = *os.Stdout
+	if _, err := out.Write(src); err != nil {
+		log.Fatal(err)
+	}
 }
