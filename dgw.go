0d0603d9ba880850ccab653a938534bd716acb81
Akira Chiku <akira.chiku@gmail.com>
2017-06-24 16:45:07 +0900

    Remove some files

--- dgw.go
+++ dgw.go
@@ -11,8 +11,25 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/achiku/varfmt"
+	_ "github.com/lib/pq" // postgres
 	"github.com/pkg/errors"
 )
+
+// Queryer database/sql compatible query interface
+type Queryer interface {
+	Exec(string, ...interface{}) (sql.Result, error)
+	Query(string, ...interface{}) (*sql.Rows, error)
+	QueryRow(string, ...interface{}) *sql.Row
+}
+
+// OpenDB opens database connection
+func OpenDB(connStr string) (*sql.DB, error) {
+	conn, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to database")
+	}
+	return conn, nil
+}
 
 const pgLoadColumnDef = `
 SELECT
